b2164c2e8295ae4bf5947ee7e4d12bcf7bd8db39
ossamakada75 <ossamakada75@gmail.com>
2024-03-28 23:58:09 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
+
+
 if __name__ == "__main__":
+
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as resp:
         content = resp.read()
